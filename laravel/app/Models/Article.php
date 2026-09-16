867cccc4ffcb40a3f37299be11966aaa30a5f556
Blaise Mounix <bmounetchoubou@gmail.com>
2026-09-16 16:20:24 +0000

    feat(laravel): ajout des models de données

---
+++ laravel/app/Models/Article.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'author',
+        'notify_subscribers',
+    ];
+
+
+    public function scopeSearch($query, $term)
+    {
+        return $query->whereRaw(
+            "to_tsvector('french', title || ' ' || content) @@ plainto_tsquery('french', ?)",
+            [$term]
+        );
+    }
+
+    public function images()
+    {
+        return $this->hasMany(ArticleImage::class);
+    }
+}
